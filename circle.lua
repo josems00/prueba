15339c33f548f4d59807686bdfd5a1ce29aedcef
alfonsofernandezaybar <alfonsofernandezaybar@gmail.com>
2025-04-22 12:14:01 +0200

    Update circle.lua

--- circle.lua
+++ circle.lua
@@ -1,52 +1,50 @@
--- Definir los puntos
-p1 = Point(100, 200)
-p2 = Point(200, 100)
-p3 = Point(300, 200)
-
--- Dibujar puntos
-color(255, 0, 0) draw(p1)
-color(0, 255, 0) draw(p2)
-color(0, 0, 255) draw(p3)
+-- Crear puntos
+local p1 = Point(0, 0)
+local p2 = Point(10, 0)
+local p3 = Point(5, 8)
 
 -- Crear segmentos
-s1 = Segment(p1, p2)
-s2 = Segment(p2, p3)
+local s1 = Segment(p1, p2)
+local s2 = Segment(p2, p3)
+
+-- Puntos medios
+local m1 = s1.MiddlePoint
+local m2 = s2.MiddlePoint
+
+-- Normales usando Line.Normal
+local n1 = s1.Line.Normal
+local n2 = s2.Line.Normal
+
+-- Líneas perpendiculares a los segmentos en sus puntos medios
+local l1 = lineFrom(n1, m1)
+local l2 = lineFrom(n2, m2)
+
+-- Centro del círculo (intersección de las mediatrices)
+local c = intersection(l1, l2)
+
+-- Radio: distancia del centro a uno de los puntos
+local r = c:Distance(p1)
+
+-- Dibujar puntos
+color(255, 0, 0)   -- rojo
+width(2)
+draw(p1)
+draw(p2)
+draw(p3)
 
 -- Dibujar segmentos
-color(0, 0, 0) width(1.5)
+color(0, 0, 255)   -- azul
+width(1)
 draw(s1)
 draw(s2)
 
--- Calcular puntos medios
-m1 = Point((p1.X + p2.X)/2, (p1.Y + p2.Y)/2)
-m2 = Point((p2.X + p3.X)/2, (p2.Y + p3.Y)/2)
-
--- Calcular vectores normales
-v1 = Vector(p2.X - p1.X, p2.Y - p1.Y):Normal():Rotate90()
-v2 = Vector(p3.X - p2.X, p3.Y - p2.Y):Normal():Rotate90()
-
--- Dibujar líneas perpendiculares
-l1 = Line(m1, v1)
-l2 = Line(m2, v2)
-
-color(0, 0, 255) width(0.8)
+-- Dibujar mediatrices
+color(0, 128, 0)   -- verde
+width(1)
 draw(l1)
 draw(l2)
 
--- Intersección de líneas: centro del círculo
-c = l1:Intersect(l2)
-
--- Dibujar centro
-color(255, 0, 255) width(3)
-draw(c)
-
--- Calcular radio (distancia entre centro y p1)
-r = Vector(c.X - p1.X, c.Y - p1.Y):Norm()
-
--- Dibujar círculo
-color(255, 165, 0) width(1.5)
+-- Dibujar el círculo
+color(0, 0, 0)     -- negro
+width(2)
 draw(Circle(c, r))
-
--- Mostrar coordenadas
-print("Centro: (" .. c.X .. ", " .. c.Y .. ")")
-print("Radio: " .. r)
